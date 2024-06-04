e6d4830e8a59af015849e4398075c6566c201843
Magiesita <161981915+Magiesita@users.noreply.github.com>
2024-06-04 11:15:04 -0700

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,54 @@
+let questions = [{
+        numb: 1,
+        question: "¿Que caracteristicas hacen que una casa sea considerada ecologica?",
+        answer: "Todas las anteriores",
+        options: [
+            "Uso de materiales reciclados",
+            "Eficiencia energetica",
+            "Diseño bioclimatico",
+            "Todas las anteriores"
+        ]
+    },
+    {
+        numb: 2,
+        question: ". ¿Qué componente esencial suele incluir una casa ecologica para maximizar la eficiencia energetica?",
+        answer: "Aislamiento adecuado y ventanas de doble panel",
+        options: [
+            "Ventanas simples con marcos de metal",
+            "Aislamiento deficiente en las paredes",
+            "Sistemas de calefaccion y refrigeracion convencionales",
+            "Aislamiento adecuado y ventanas de doble panel"
+        ]
+    },
+    {
+        numb: 3,
+        question: "¿Qué tipo de jardin seria mas compatible con una casa ecologica?",
+        answer: "Jardin de xeriscaping con plantas",
+        options: [
+            "Jardin con cesped sintetico",
+            "Jardin de xeriscaping con plantas",
+            "Jardin con cesped natural regado diariamente"
+        ]
+    },
+    {
+        numb: 4,
+        question: "¿Cual es uno de los principales desafios al construir una casa ecologica?",
+        answer: "Costo inicial mas elevado debido a la implementacion de caracteristicas ecologicas",
+        options: [
+            "Limitaciones en la disponibilidad de tecnologias sostenibles y eficientes",
+            "Costo inicial mas elevado debido a la implementacion de caracteristicas ecologicas",
+            "Dificultades para obtener permisos de construccion debido a regulaciones ambientales",
+            "Resistencia por parte de los contratistas y arquitectos a adoptar practicas de construccion sostenible"
+        ]
+    },
+    {
+        numb: 5,
+        question: "¿Cual es un ejemplo de diseño pasivo que se puede implementar en una casa ecologica para reducir la necesidad de calefaccion?",
+        answer: "Orientar las ventanas hacia el sur para aprovechar la luz solar",
+        options: [
+            "No aislar adecuadamente las paredes y techos",
+            "Orientar las ventanas hacia el sur para aprovechar la luz solar",
+            "Utilizar materiales de construccion no sostenibles"
+        ]
+    },
+];
